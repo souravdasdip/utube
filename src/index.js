5c072ad732e7a2a86c36ce8144f87091edd2c41f
Sourav Das <souravdasdip65@gmail.com>
2024-02-19 18:33:51 +0600

    express app routing after connect to db

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import { app } from './app.js';
 import connectDB from './db/index.js';
 
 dotenv.config({
@@ -6,3 +7,11 @@ dotenv.config({
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.POST || 8000, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`);
+        })
+    })
+    .catch(err => {
+        console.log("MongoDB connenction failed!!", err);
+    })
