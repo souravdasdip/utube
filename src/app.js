4da1dac4c384016011a661604c09e69d1d9d51b3
Sourav Das <souravdasdip65@gmail.com>
2024-02-19 19:56:09 +0600

    utils code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from "express";
 
 const app = express()
 
+//which origin can access our backend
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}))
+
+//json handled and limit its size
+app.use(express.json({ limit: "16kb" }))
+
+//url handled with params
+app.use(express.json({ limit: "16kb" }))
+
+//to access the public folder for file store or to get the file
+app.use(express.static("public"))
+
+//to access cookie from user browser and set the cookies
+app.use(cookieParser())
+
+
 
 
 export { app };
+
