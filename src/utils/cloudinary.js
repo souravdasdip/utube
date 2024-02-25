f1247c7552fc4e76a95a71fdf3cda37517fa225f
Sourav Das <souravdasdip65@gmail.com>
2024-02-25 16:24:05 +0600

    file upload cloudinary function

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SERCRET
+})
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(
+            localFilePath,
+            {
+                resource_type: "auto"
+            }
+        )
+
+        return response
+    } catch (error) {
+
+        //remove the locally save temporary file as upload operation got failed
+        fs.unlinkSync(localFilePath)
+    }
+}
+
+
+export { uploadOnCloudinary }
+
