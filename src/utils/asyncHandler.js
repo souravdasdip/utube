2c0577e24c19a91043a651f1b6e3383a8cdc5eaa
Sourav Das <souravdasdip65@gmail.com>
2024-02-25 14:59:19 +0600

    asyncHandler.js implemented

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,9 +1,23 @@
-// Handle Async operation
+// Handle Async operation with Promise
 const asyncHandler = (requestHandler) => {
     (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
+        Promise
+            .resolve(requestHandler(req, res, next))
+            .catch((error) => next(error))
     }
 }
 
-export { asyncHandler }
+//Handle Async operation with try/catch
+const anotherAsyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export { anotherAsyncHandler, asyncHandler }
 
