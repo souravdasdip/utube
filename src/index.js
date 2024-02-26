531f2b1735632c85405d34de1776e3300db6f86b
Sourav Das <souravdasdip65@gmail.com>
2024-02-26 11:58:30 +0600

    fixing index page the port

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ dotenv.config({
 
 connectDB()
     .then(() => {
-        app.listen(process.env.POST || 8000, () => {
+        app.listen(process.env.PORT || 8000, () => {
             console.log(`Server is running at port: ${process.env.PORT}`);
         })
     })
