bdd588e933cded0706e9e2afed59fed681377fc6
souravdasdip <souravdasdip65@gmail.com>
2024-03-26 19:13:18 +0600

    Get User Channel Profile With aggregation pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -325,5 +325,74 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
 
 })
 
-export { updateUserAvatar, updateUserCoverImage, updateAccountDetails, getCurrentUser, changeCurrentPassword, loginUser, logoutUser, registerUser, refreshAccessToken };
+
+//subscriber and subscribe to find
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+    const { username } = req.query
+    
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is missing!")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },{
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },{
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },{
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },{
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel does not exist!")
+    }
+
+    console.log({channel});
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, channel[0], "Channel info fetched!"))
+})
+
+export { getUserChannelProfile, updateUserAvatar, updateUserCoverImage, updateAccountDetails, getCurrentUser, changeCurrentPassword, loginUser, logoutUser, registerUser, refreshAccessToken };
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
@@ -36,6 +36,7 @@ router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/user-channel-profile").get(verifyJWT, getUserChannelProfile)
 
 
 
